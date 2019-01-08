edcf8290fadde9c1d38618206aa55240a960777d
Joshua Vance <jvancehomefolder@Joshuas-MBP.fios-router.home>
2019-01-08 13:39:20 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+console.log("Up and Running!");
+
+var cardOne = "Queen";
+var cardTwo = "King";
+var	cardThree = "King";
+var	cardFour = "Queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+
